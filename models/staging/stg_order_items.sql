21ec072c843760882fd59aee115e0be342423a2e
Adamb7rr <bahrcr7@gmail.com>
2026-05-02 19:36:42 +0300

    Fix formatting in stg_order_items.sql for consistent spacing

--- models/staging/stg_order_items.sql
+++ models/staging/stg_order_items.sql
@@ -3,6 +3,6 @@ id AS item_id,
 order_id,
 product_id,
 quantity,
-unit_price,
-(quantity * unit_price) AS total_price
-FROM {{source('raw_data', 'order_items')}}
+unit_price, 
+(quantity * unit_price) AS total_price 
+FROM {{ source('raw_data', 'order_items') }}
